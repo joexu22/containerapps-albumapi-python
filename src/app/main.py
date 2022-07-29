b956c126f87fa5b8d97f1271b7d19580712ddf42
Xu, Joseph [ETHUS NON-J&J] <JXu122@its.jnj.com>
2022-07-29 07:06:00 -0400

    arrangement

--- src/app/main.py
+++ src/app/main.py
@@ -25,10 +25,10 @@ class Album():
 albums = [
     Album(1, "Nimrod", "Green Day", "Good Riddance (Time Of Your Life)", "https://m.media-amazon.com/images/I/71dBwWTWgKL._SX425_.jpg", "https://www.youtube.com/watch?v=CnQ8N1KacJc"),
     Album(2, "Breakfast in America", "Supertramp", "Goodbye Stranger", "https://m.media-amazon.com/images/I/51Xs5nXws8L._UX358_FMwebp_QL85_.jpg", "https://www.youtube.com/watch?v=u8pVZ5hTGJQ"),
-    Album(3, "Time at JnJ", "Xu Labels", "I'll Miss Y'all", "https://image.shutterstock.com/image-vector/chinese-calligraphy-translation-xu-a-260nw-1773157127.jpg", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
-    Album(4, "No Strings Attached", "NSYNC", "Bye Bye Bye", "https://m.media-amazon.com/images/I/812H+OFXlCL._SX425_.jpg", "https://www.youtube.com/watch?v=Eo-KmOd3i7s"),
-    Album(5, "Hit the Road Jack", "Ray Charles", "Hit the Road Jack", "https://images-na.ssl-images-amazon.com/images/I/51yihgHF-5L._SY300_SX300_QL70_FMwebp_.jpg", "https://www.youtube.com/watch?v=uSiHqxgE2d0"),
-    Album(6, "Making Mirrors", "Gotye", "Somebody That I Used to Know", "https://m.media-amazon.com/images/I/91vXYllHodL._SX425_.jpg", "https://www.youtube.com/watch?v=8UVNT4wvIGY"),    
+    Album(3, "No Strings Attached", "NSYNC", "Bye Bye Bye", "https://m.media-amazon.com/images/I/812H+OFXlCL._SX425_.jpg", "https://www.youtube.com/watch?v=Eo-KmOd3i7s"),
+    Album(4, "Hit the Road Jack", "Ray Charles", "Hit the Road Jack", "https://images-na.ssl-images-amazon.com/images/I/51yihgHF-5L._SY300_SX300_QL70_FMwebp_.jpg", "https://www.youtube.com/watch?v=uSiHqxgE2d0"),
+    Album(5, "Making Mirrors", "Gotye", "Somebody That I Used to Know", "https://m.media-amazon.com/images/I/91vXYllHodL._SX425_.jpg", "https://www.youtube.com/watch?v=8UVNT4wvIGY"),    
+    Album(6, "Time at JnJ", "Xu Labels", "I'll Miss Y'all", "https://image.shutterstock.com/image-vector/chinese-calligraphy-translation-xu-a-260nw-1773157127.jpg", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
     Album(7, "Feeling Strangely Fine", "Semisonic", "Closing Time", "https://images-na.ssl-images-amazon.com/images/I/61nyQCkciLL._SX300_SY300_QL70_FMwebp_.jpg", "https://www.youtube.com/watch?v=xGytDsqkQY8"),
 ]
 
