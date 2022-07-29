8bd36b18712f5459104592f975aa6d3ca06314ee
Xu, Joseph [ETHUS NON-J&J] <JXu122@its.jnj.com>
2022-07-28 20:36:17 -0400

    test

--- src/app/main.py
+++ src/app/main.py
@@ -14,20 +14,17 @@ app.add_middleware(
 )
 
 class Album():
-    def __init__(self, id, title, artist, price, image_url):
+    def __init__(self, id, title, artist, price, image_url, link):
          self.id = id
          self.title = title
          self.artist = artist
          self.price = price
          self.image_url = image_url
+         self.link = link
 
 albums = [ 
-    Album(1, "You, Me and an App Id", "Daprize", 10.99, "https://aka.ms/albums-daprlogo"),
-    Album(2, "Seven Revision Army", "The Blue-Green Stripes", 13.99, "https://aka.ms/albums-containerappslogo"),
-    Album(3, "Scale It Up", "KEDA Club", 13.99, "https://aka.ms/albums-kedalogo"),
-    Album(4, "Lost in Translation", "MegaDNS", 12.99,"https://aka.ms/albums-envoylogo"),
-    Album(5, "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
-    Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo")
+    Album(1, "Feeling Strangely Fine", "Semisonic", "Closing Time", "https://images-na.ssl-images-amazon.com/images/I/61nyQCkciLL._SX300_SY300_QL70_FMwebp_.jpg", "https://www.youtube.com/watch?v=xGytDsqkQY8"),
+    Album(2, "Nimrod", "Green Day", "Green Day - Good Riddance (Time Of Your Life)", "https://m.media-amazon.com/images/I/71dBwWTWgKL._SX425_.jpg", "https://www.youtube.com/watch?v=CnQ8N1KacJc"),
 ]
 
 @app.get("/")
